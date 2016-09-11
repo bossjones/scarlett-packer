c1a84764611f52498338fba0d1bfde8d7f3de679
Malcolm Jones <malcolm@adobe.com>
2016-09-11 15:51:04 -0400

    chg: preseed

--- scripts/ubuntu/preseed.sh
+++ scripts/ubuntu/preseed.sh
@@ -8,9 +8,9 @@ echo "sed -i -e 's/^in-target.*tasksel.*/#\\0/' /var/lib/dpkg/info/pkgsel.postin
 chmod +x /usr/lib/post-base-installer.d/90skip-tasksel
 
 
-echo 'Defaults:pi !requiretty' > /target/etc/sudoers.d/pi               && \
-echo 'pi ALL=(ALL) NOPASSWD: ALL' >> /target/etc/sudoers.d/pi            && \
-echo '%pi ALL=(ALL) NOPASSWD: ALL' >> /target/etc/sudoers.d/pi            && \
-chmod 440 /target/etc/sudoers.d/pi                                       && \
-ln -sf /dev/null /target/etc/systemd/network/99-default.link             && \
+echo 'Defaults:pi !requiretty' > /etc/sudoers.d/pi               && \
+echo 'pi ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/pi            && \
+echo '%pi ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/pi            && \
+chmod 440 /etc/sudoers.d/pi                                       && \
+ln -sf /dev/null /etc/systemd/network/99-default.link             && \
 update-initramfs -u
